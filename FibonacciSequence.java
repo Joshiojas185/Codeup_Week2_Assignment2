daa1181d6318952540731b421c125aca34c8b2ec
Ojas Joshi <146631578+Joshiojas185@users.noreply.github.com>
2024-09-12 10:22:45 +0530

    Add files via upload

---
+++ FibonacciSequence.java
@@ -0,0 +1,46 @@
+/*
+In this class, we will implement a method to calculate the Nth Fibonacci Sequence using recursion
+Program Owner -> Ojas Joshi
+Date -> 11/09/2024
+ */
+import java.util.Scanner;
+public class FibonacciSequence {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println(Constants.INPUT_SEQUENCE);
+        try {
+            int count = scan.nextInt();
+        boolean check  = true ;
+        if(count == 1 || count == 2){
+            System.out.print(Constants.DESIRED_OUTPUT);
+            System.out.println(1);
+            check = false;
+        }
+        if(count <= 0){
+            System.out.println(Constants.INVALID_INPUT);
+            check = false;
+        }
+        if(check) {
+            count = count - 2;
+            System.out.print(Constants.DESIRED_OUTPUT);
+            System.out.println(fibonacciSequence(count, 1, 1));
+        }
+        }
+        catch(Exception e){
+            System.out.println(Constants.INVALID_INPUT);
+        }
+    }
+    // Returns the Nth Fibonacci Number from Fibonacci Sequence
+    // Input -> Integer
+    // Return type -> Integer
+    public static int fibonacciSequence(int count, int first , int second){
+        int third = first + second;
+        first = second;
+        second = third ;
+        count-- ;
+        if(count == 0){
+            return third;
+        }
+        return fibonacciSequence(count,first,second);
+    }
+}
